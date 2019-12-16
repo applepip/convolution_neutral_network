827e045127821d93b40a26ab0862df2b77455436
leon <applepip@163.com>
2019-12-16 17:38:08 +0800

    网络中的网络

---
+++ nin/nin_training.py
@@ -0,0 +1,42 @@
+# NiN块是NiN中的基础块。它由一个卷积层加两个充当全连接层的 1×1 卷积层串联而成。
+# # # 其中第一个卷积层的超参数可以自行设置，而第二和第三个卷积层的超参数一般是固定的。
+
+from mxnet import gluon, init, nd
+from mxnet.gluon import nn
+
+def nin_block(num_channels, kernel_size, strides, padding):
+    blk = nn.Sequential()
+    blk.add(nn.Conv2D(num_channels, kernel_size,
+                      strides, padding, activation='relu'),
+            nn.Conv2D(num_channels, kernel_size=1, activation='relu'),
+            nn.Conv2D(num_channels, kernel_size=1, activation='relu'))
+    return blk
+
+net = nn.Sequential()
+net.add(nin_block(96, kernel_size=11, strides=4, padding=0),
+        nn.MaxPool2D(pool_size=3, strides=2),
+        nin_block(256, kernel_size=5, strides=1, padding=2),
+        nn.MaxPool2D(pool_size=3, strides=2),
+        nin_block(384, kernel_size=3, strides=1, padding=1),
+        nn.MaxPool2D(pool_size=3, strides=2), nn.Dropout(0.5),
+        # 标签类别数是10
+        nin_block(10, kernel_size=3, strides=1, padding=1),
+        # 全局平均池化层将窗口形状自动设置成输入的高和宽
+        nn.GlobalAvgPool2D(),
+        # 将四维的输出转成二维的输出，其形状为(批量大小, 10)
+        nn.Flatten())
+
+X = nd.random.uniform(shape=(1, 1, 224, 224))
+net.initialize()
+for layer in net:
+    X = layer(X)
+    print(layer.name, 'output shape:\t', X.shape)
+
+from nin_prepare import *
+
+lr, num_epochs, batch_size, ctx = 0.1, 5, 128, try_gpu()
+net.initialize(force_reinit=True, ctx=ctx, init=init.Xavier())
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr})
+train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=224)
+train_ch5(net, train_iter, test_iter, batch_size, trainer, ctx,
+              num_epochs)
