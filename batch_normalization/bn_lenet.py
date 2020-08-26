ceb2385021df5cab70e21b2cc4b7face5b0b3822
renliang <renliang@polarico.com>
2020-08-26 18:26:46 +0800

    update

---
+++ batch_normalization/bn_lenet.py
@@ -0,0 +1,27 @@
+from batch_normalization import *
+from function_tools import *
+
+net = nn.Sequential()
+net.add(nn.Conv2D(6, kernel_size=5),
+        BatchNorm(6, num_dims=4),
+        nn.Activation('sigmoid'),
+        nn.MaxPool2D(pool_size=2, strides=2),
+        nn.Conv2D(16, kernel_size=5),
+        BatchNorm(16, num_dims=4),
+        nn.Activation('sigmoid'),
+        nn.MaxPool2D(pool_size=2, strides=2),
+        nn.Dense(120),
+        BatchNorm(120, num_dims=2),
+        nn.Activation('sigmoid'),
+        nn.Dense(84),
+        BatchNorm(84, num_dims=2),
+        nn.Activation('sigmoid'),
+        nn.Dense(10))
+
+
+lr, num_epochs, batch_size, ctx = 1.0, 5, 256, try_gpu()
+net.initialize(ctx=ctx, init=init.Xavier())
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr})
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+train_ch5(net, train_iter, test_iter, batch_size, trainer, ctx,
+              num_epochs)
